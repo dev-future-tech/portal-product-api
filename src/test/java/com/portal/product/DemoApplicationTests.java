1801e351d1bf881c0ea26a0de03a62a01c074c52
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-08-26 08:24:29 -0700

    Initial add and push

---
+++ src/test/java/com/portal/product/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
